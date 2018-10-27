89ce6ab56bd328304768b97ed4c21d92288e6778
Hui-Jou Chou <alwaysrejoice516@gmail.com>
2018-10-26 20:09:08 -0400

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,6 @@
 import uuid from 'uuid';
 import  database  from '../firebase/firebase';
+import expenses from '../tests/fixtures/expenses';
 //add_expense
 export const addExpense = (expense) =>({
     type: 'ADD_EXPENSE',
@@ -33,3 +34,24 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+//set_expenses
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
