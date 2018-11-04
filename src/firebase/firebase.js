126ec3c9a52daaa7e430143d01dbcad6149ad94e
Hui-Jou Chou <alwaysrejoice516@gmail.com>
2018-11-04 16:32:44 -0500

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,7 +10,9 @@ const config = {
   };
   firebase.initializeApp(config);
 const database = firebase.database();
-export {firebase, database as default};
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {firebase, googleAuthProvider, database as default};
 /*database.ref('expenses').once('value').then((snapshot) =>{
     const expenses = [];
     snapshot.forEach((childSnapshot) => {
